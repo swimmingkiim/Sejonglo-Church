7698e59701ac7de0489e8c8767a83a0a02de9ffc
swimmingkiim <swimmingkiim@gmail.com>
2020-07-13 00:00:05 +0900

    init - webpack, babel, react

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sejonglo Church
---
+++ app/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>mic test one two one two</h1>
+    </div>
+  );
+};
+
+export default App;
