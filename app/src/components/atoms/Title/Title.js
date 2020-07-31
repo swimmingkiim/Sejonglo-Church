5a2aea22d217307497fee29bb5f162436500b1c1
swimmingkiim <swimmingkiim@gmail.com>
2020-07-31 14:34:28 +0900

    edit Title style

--- app/src/components/atoms/Title/Title.js
+++ app/src/components/atoms/Title/Title.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 import * as Style from "./style";
 
 const Title = ({ content, size }) => {
-  let fontSize = "250%";
-  if (size === "big") fontSize = "300%";
-  else if (size === "small") fontSize = "200%";
+  let fontSize = 250;
+  if (size === "big") fontSize = 300;
+  else if (size === "small") fontSize = 200;
 
   return <Style.Title {...{ fontSize }}>{content}</Style.Title>;
 };
--- app/src/components/atoms/Title/style.js
+++ app/src/components/atoms/Title/style.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 export const Title = styled.h1`
-  font-size: ${(props) => props.fontSize};
+  font-size: ${(props) => props.fontSize}%;
   color: #000000;
   letter-spacing: 0.1em;
+
+  @media only screen and (max-width: 768px) {
+    font-size: ${(props) => props.fontSize - 150}%;
+  }
 `;
